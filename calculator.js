1f36df9459d30b029464230d53ac8668ee4f4e6b
JAU <javier.aguilaht@gmail.com>
2024-01-26 15:59:06 -0300

    the calculator has been updated to a new estructure of js, incorporating a class and functions like display, appendnumber, chooseoperation

--- calculator.js
+++ calculator.js
@@ -1,34 +1,72 @@
+    //selectors
+const currentTextValue = document.querySelector('.current-value')
+const previusTextValue = document.querySelector('.previus-value')
+const numberButtons = document.querySelectorAll('[number-buttons]');
+const operators = document.querySelectorAll('.sign');
+const equalsButton = document.querySelector('.equals');
+const display = document.querySelector('.display');
 
 
-const operations = {
-        sum: function(a , b){ return a + b},
-        sub: function(a, b){return a - b},
-        mult: function(a, b){return a * b},
-        div: function(a, b){return a/b}
+class Calculator {
+    constructor(currentTextValue, previusTextValue){
+        this.currentTextValue = currentTextValue;
+        this.previusTextValue = previusTextValue;
+        this.clearAll();
+    
+    }
 
-};
+    
+    clearAll(){
+        this.currentValue = ''
+        this.previusValue = ''
+        this.operation = undefined
+    } 
+    
+    updateDisplay(){
+        this.currentTextValue.innerText = this.currentValue
+        this.previusTextValue.innerText = this.previusValue
+        
+    }
+    appendNumber(number){
+        if (number === '.' & this.currentValue.includes('.')) return
+        this.currentValue = this.currentValue.toString() + number.toString()
+        
+    }
+    chooseOperation(operation){
+        if(this.currentValue === '')return
+        this.operation = operation
+        this.previusValue = this.currentValue
+        this.currentValue = '';
+    }
 
+    compute(){}
+    
+    
+    
+    
+    
+}
 
+const calculator = new Calculator (currentTextValue, previusTextValue)
 
-//selectors
-
-let buttons = document.querySelectorAll('.button');
-let display = document.querySelector('.display');
-
-
-//listeners 
-let newVAlue = [];
-
-buttons.forEach(button => {
-
+numberButtons.forEach(button => {
+    
     button.addEventListener('click', ()=>{
         
-        newVAlue.push(button.value).join('');
-
-        display.innerText = newVAlue;
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay()
         
- 
+    })
     
-    });
+})
+
+operators.forEach(operator => {
     
-});
+    operator.addEventListener('click', ()=>{
+        
+        calculator.chooseOperation(operator.innerText);
+        calculator.updateDisplay()
+        
+    })
+    
+})
